d40a2dcff1e4142dbf03c849cca86a167b83eef5
João Damaceno <joaodamaceno188@gmail.com>
2022-02-27 19:15:33 -0400

    Criado uma View pasta: Funcionario e Layout. Funcionario Controller: Adicionado a variavel funcionario todos os dados do Model respectivo

---
+++ crud-laravel/app/Http/Controllers/FuncionarioController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Funcionario;
+
+class FuncionarioController extends Controller
+{
+    public function index()
+    {
+        $funcionario = Funcionario::all();
+
+        return view('funcionario.funcionarioIndex', ['funcionario'=>$funcionario]);
+    }
+}
