19d679c281e735a31e79228c9845fbee3c7476dd
João Damaceno <joaodamaceno188@gmail.com>
2022-02-28 04:59:38 -0300

    Mudanças feitas na indexFuncionario

--- crud-laravel/resources/views/funcionario/indexFuncionario.blade.php
+++ crud-laravel/resources/views/funcionario/indexFuncionario.blade.php
@@ -4,8 +4,11 @@
   <div class="grid grid-cols-1 lg:grid-cols-2">
     <div class="bg-red-600 lg:min-h-screen lg:flex lg:items-center p-8 sm:p-12">
       <div class="flex-grow">
-        <h1 class="text-white text-center text-xl">Faça o Cadastro para Continuar!</h1>
+        <h1 class="text-white text-center text-xl">Seja Bem-vindo</h1>
+        <h2 class="text-white text-center text-xl font-bold">Entre para continuar</h2>
       </div>
+      <h1> ASDASD </h1>
     </div>
+
   </div>
 @endsection
