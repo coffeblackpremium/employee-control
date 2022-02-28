5b183505a6f336682fd3eb5b2be716a10706510c
João Damaceno <joaodamaceno188@gmail.com>
2022-02-27 20:13:55 -0400

    Retirado a Navbar em Main.blade

--- crud-laravel/resources/views/funcionario/indexFuncionario.blade.php
+++ crud-laravel/resources/views/funcionario/indexFuncionario.blade.php
@@ -1,4 +1,11 @@
 @extends('layout.main')
+
 @section('content')
-  
+  <div class="grid grid-cols-1 lg:grid-cols-2">
+    <div class="bg-red-600 lg:min-h-screen lg:flex lg:items-center p-8 sm:p-12">
+      <div class="flex-grow">
+        <h1 class="text-white text-center text-xl">Faça o Cadastro para Continuar!</h1>
+      </div>
+    </div>
+  </div>
 @endsection
--- crud-laravel/resources/views/layout/main.blade.php
+++ crud-laravel/resources/views/layout/main.blade.php
@@ -9,24 +9,6 @@
     <title>@yield('title')</title>
   </head>
   <body class="bg-indigo-100">
-  
-    <nav class="flex items-center justify-between flex-wrap bg-violet-500 shadow-lg p-6">
-      <div class="flex items-center flex-shrink-0 text- mr-6">
-        <span class="font-semibold text-lg tracking-tight">CRUD Laravel</span>
-      </div>
-      <div class="w-full block flex-grow lg:items-center lg:w-auto">
-        <div class="flex justify-center text-sm lg:flex-grow">
-          <a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-slate-200 font-bold font-sans:Roboto hover:text-slate-300 mr-6">
-            Home
-          </a>
-          <a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-neutral-200 font-bold hover:text-slate-300 mr-4">
-            Tabela
-          </a>
-        </div>
-      </div>
-    </nav>
-      
-    </nav>
     <main>
       @yield('content')
     </main>
